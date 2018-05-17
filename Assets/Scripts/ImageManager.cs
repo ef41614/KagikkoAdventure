92f52818cf23a3a623749f0b9f3f36a8c0808c35
Atsushi.Iida <ef41614@gmail.com>
2018-05-17 10:39:42 +0900

    KagikkoAdventureプロジェクト 21回目のコミット
    オリジナルゲーム：キャラFaceアイコンを左下に表示。まだ1Pのみ。

---
+++ Assets/Scripts/ImageManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageManager : MonoBehaviour {
+
+	public Sprite 画像1;
+	public Sprite 画像2;
+
+	private GameObject obj;
+	private Image image;
+
+	//☆################☆################  Start  ################☆################☆
+
+	void Start () {
+		obj = GameObject.Find("image(imageオブジェクトの名前)").gameObject as GameObject;
+		image = obj.GetComponent<Image> ();
+
+	}
+
+
+	//####################################  Update  ###################################
+
+	void Update () {
+
+
+	}
+
+	//####################################  other  ####################################
+
+	public void 画像変更(){
+		image.sprite = 画像2;
+	}
+
+	//#################################################################################
+
+}
+// End
